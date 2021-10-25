9c857122c88f12d758aa01c4b0f7f7dfff1a4f48
Tyler Long <tyler4long@gmail.com>
2021-10-25 12:57:14 -0700

    Add @rc-ex/ws

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import RingCentral from '@rc-ex/core';
 import SDK from '@ringcentral/sdk';
 import RcSdkExtension from '@rc-ex/rcsdk';
 import DebugExtension from '@rc-ex/debug';
+import WebSocketExtension from '@rc-ex/ws';
 
 const sdk = new SDK({
   clientId: process.env.RINGCENTRAL_CLIENT_ID!,
@@ -17,6 +18,10 @@ const sdk = new SDK({
   const rc = new RingCentral();
   const rcSdkExtension = new RcSdkExtension({rcSdk: sdk});
   await rc.installExtension(rcSdkExtension);
+  const webSocketExtension = new WebSocketExtension({
+    restOverWebSocket: true,
+  });
+  await rc.installExtension(webSocketExtension);
   const debugExtension = new DebugExtension();
   await rc.installExtension(debugExtension);
   const extInfo = await rc.restapi().account().extension().get();
